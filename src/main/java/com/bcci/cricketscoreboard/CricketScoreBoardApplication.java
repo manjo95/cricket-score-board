949ea1eea33271dd4b1222d3f06e81648025ea42
manjo95 <selva.manoj@gmail.com>
2021-01-23 17:21:19 +0000

    Initial Commit;

---
+++ src/main/java/com/bcci/cricketscoreboard/CricketScoreBoardApplication.java
@@ -0,0 +1,13 @@
+package com.bcci.cricketscoreboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketScoreBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CricketScoreBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bcci/cricketscoreboard/CricketScoreBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcci.cricketscoreboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricketScoreBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
